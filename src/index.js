b5802b06c72392e4406c4fa69d05405b17b21ebe
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-14 17:01:58 +0100

    OAuth

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import LogInicioSesion from './pages/logsInicioSesion';
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+             <Route path="/" element={<App />} />
+             <Route path="/logsInicioSesion" element={<LogInicioSesion />} />
+      </Routes>
+
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
